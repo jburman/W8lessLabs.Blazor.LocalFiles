2bb6b9b58b4d03f129b295a6e9ca1e5cbd63403d
Jeremy Burman <jburman@gmail.com>
2018-11-27 21:04:49 -0500

    Provide access to file blob urls. Add example of rendering an image from a blob url.

--- src/Blazor.LocalFiles/SelectedFileReader.cs
+++ src/Blazor.LocalFiles/SelectedFileReader.cs
@@ -8,36 +8,34 @@ namespace Blazor.LocalFiles
     public class SelectedFileReader : IDisposable
     {
         private HttpClient _http;
-        private string _fileBlobUrl;
         private CustomBlobFetch _customBlobFetch;
-        private Func<SelectedFile, Task<string>> _fileCreator;
-        private Func<string, Task> _fileDestructor;
+        private FileBlobUrls _fileBlobUrls;
 
         private bool _disposed;
 
         public SelectedFileReader(HttpClient http,
             SelectedFile file, 
-            Func<SelectedFile, Task<string>> fileCreator, 
-            Func<string, Task> fileDestructor)
+            FileBlobUrls fileBlobUrls)
         {
             _http = http ?? throw new ArgumentNullException(nameof(http));
             File = file ?? throw new ArgumentNullException(nameof(file));
-            _fileCreator = fileCreator ?? throw new ArgumentNullException(nameof(fileCreator));
-            _fileDestructor = fileDestructor ?? throw new ArgumentNullException(nameof(fileDestructor));
+            _fileBlobUrls = fileBlobUrls ?? throw new ArgumentNullException(nameof(fileBlobUrls));
         }
 
         public SelectedFile File { get; private set; }
 
+        public async Task<string> GetFileBlobUrlAsync() => await _fileBlobUrls.GetFileBlobUrl(File.Name);
+
         public async Task<byte[]> GetFileBytesAsync()
         {
-            await _PrepareCustomBlobFetchAsync();
-            return await _http.GetByteArrayAsync(_customBlobFetch.CustomBlobUrl);
+            string customBlobUrl = await _GetCustomBlobFetchAsync();
+            return await _http.GetByteArrayAsync(customBlobUrl);
         }
 
         public async Task<Stream> GetFileStreamAsync()
         {
-            await _PrepareCustomBlobFetchAsync();
-            return await _http.GetStreamAsync(_customBlobFetch.CustomBlobUrl);
+            string customBlobUrl = await _GetCustomBlobFetchAsync();
+            return await _http.GetStreamAsync(customBlobUrl);
         }
 
         public void Dispose()
@@ -45,24 +43,16 @@ namespace Blazor.LocalFiles
             if (!_disposed)
             {
                 _disposed = true;
-
-                if (_customBlobFetch != null)
-                    _customBlobFetch.Dispose();
-
-                if (_fileBlobUrl != null)
-                    _fileDestructor(_fileBlobUrl);
+                _customBlobFetch?.Dispose();
+                _fileBlobUrls?.Dispose();
             }
         }
 
-        private async Task _PrepareCustomBlobFetchAsync()
+        private async Task<string> _GetCustomBlobFetchAsync()
         {
-            if (_fileBlobUrl is null)
-            {
-                _fileBlobUrl = await _fileCreator(File);
-                if (_fileBlobUrl == null)
-                    throw new NullReferenceException("Unable to create blob URL for file: " + File.Name);
-                _customBlobFetch = new CustomBlobFetch(_fileBlobUrl);
-            }
+            if (_customBlobFetch is null)
+                _customBlobFetch = new CustomBlobFetch(await GetFileBlobUrlAsync());
+            return _customBlobFetch.CustomBlobUrl;
         }
     }
 }
